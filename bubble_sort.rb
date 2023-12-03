bb7fc36ed17bdb87752c2ac607aba3148c6c4aa9
FunkyMonkey1206 <FunkyMonkey1206@users.noreply.github.com>
2023-12-03 21:53:32 +0900

    Add ruby script that includes bubble sort method

---
+++ bubble_sort.rb
@@ -0,0 +1,20 @@
+def bubble_sort(arr)
+    i = arr.length - 1
+
+    while i > 0 do
+        arr.each_index do |ind|
+            left = arr[ind]
+            right = arr[ind+1]
+            unless right == nil
+              if left > right
+                arr[ind+1] = left
+                arr[ind] = right
+              end
+            end
+        end
+    i -= 1
+    end
+return arr
+end
+
+bubble_sort([4,3,78,2,0,2])
