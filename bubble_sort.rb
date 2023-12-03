c96b56f7f1b013ac977b8887b3a772c450eab542
FunkyMonkey1206 <FunkyMonkey1206@users.noreply.github.com>
2023-12-03 22:02:25 +0900

    Add comments to several lines

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,11 +1,18 @@
 def bubble_sort(arr)
+    #setting number of iterations based on array length
     i = arr.length - 1
+    #sorting loop will run based on number of iterations
     while i > 0 do
+        #for each element in the array it will compare it to the element on the right of it
         arr.each_index do |ind|
+            #save values to variables left and right
             left = arr[ind]
             right = arr[ind+1]
+            #catches error when right value is nil(when index is more than lenigth)
             unless right == nil
+            #compare left and right values
               if left > right
+                #if left is greater than right swap values
                 arr[ind+1] = left
                 arr[ind] = right
               end
@@ -16,4 +23,5 @@ def bubble_sort(arr)
 return arr
 end
 
+#run method below to test bubble sort
 bubble_sort([4,3,78,2,0,2])
